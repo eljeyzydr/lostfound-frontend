d86e99b9bf767ebcff952cade45e74455fbf8225
ussoppqq <rizkimuh252@gmail.com>
2025-09-19 13:17:00 +0700

    Create Dashboard.php

---
+++ app/Livewire/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public function render()
+    {
+        return view('c:\xampp\kuliah\htdocs\lostfound-backend\resources\...lostfound-frontend.resources.views.dashboard');
+    }
+}
